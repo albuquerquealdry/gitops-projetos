b74c7b6264eac3dc2418e0a7d45cf90ccb363456
aldry <aldry@192.168.1.13>
2022-06-04 23:31:25 -0300

    CODE REFACTOR

--- management_envs/main.py
+++ management_envs/main.py
@@ -1,10 +1,31 @@
+from argparse import Namespace
 import os 
 import json
 
+namespace = "prod"
 component = "microservices"
+service = "RAP_API"
+
 def cloneCatalog():
-    #os.system("git clone https://github.com/albuquerquealdry/env-catalog.git") 
+    os.system("git clone https://github.com/albuquerquealdry/env-catalog.git") 
     with open(f"env-catalog/{component}.json", "r") as catalogEnvs:
-        return(catalogEnvs)
-        
-print(cloneCatalog())
+        return json.load(catalogEnvs)
+
+def checkEnvs():
+    with open (".env", "r") as env:
+        return (env.readlines())
+
+def sicronizer():
+    getEnvs = cloneCatalog()
+    envs = checkEnvs()
+    os.system("cp -f files/configmapBase.yaml configmap.yaml")
+    for env in envs:
+        env = env.replace('\n',"")
+        valuesEnv = getEnvs[f"{env}"]
+        envForEnv = valuesEnv[f"{namespace}"]
+        data = (f"""{env} : "{envForEnv}" """)
+        os.system(f""" echo "\t{data}" >> configmap.yaml """)
+        os.system(f""" sed -i "s/NAMESERVICE/{service}/g" configmap.yaml """)
+        os.system("rm -rf env-catalog ")
+
+sicronizer()
